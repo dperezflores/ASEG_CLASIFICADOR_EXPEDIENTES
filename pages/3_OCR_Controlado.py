712e7b91ddf4ffe6d626ab05d3f0981780894c83
dperezflores <daniel.apf99@gmail.com>
2026-09-21 21:49:57 -0600

    Adaptar página a navegación centralizada

--- pages/3_OCR_Controlado.py
+++ pages/3_OCR_Controlado.py
@@ -2,9 +2,7 @@ import streamlit as st
 
 from services.ocr_service import ejecutar_ocr_controlado
 from ui.common import (
-    configurar_pagina,
     mostrar_encabezado,
-    mostrar_sidebar_expediente,
     requerir_expediente,
 )
 
