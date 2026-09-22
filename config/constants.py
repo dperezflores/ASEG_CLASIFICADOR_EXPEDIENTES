c942dde5fe22c593bf27de01ded4e877ee87b97e
dperezflores <daniel.apf99@gmail.com>
2026-09-21 19:06:02 -0600

    Agregar constantes iniciales

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+APP_NAME = "Clasificador Inteligente de Expedientes"
+APP_SUBTITLE = "Identificación, codificación y organización documental de expedientes de obra pública"
+
+CATALOGO_PATH = "catalogo/CODIFICACION_DOCUMENTOS.xlsx"
+HOJAS_PROCEDIMIENTO = ("DIR", "LPU", "LSI")
