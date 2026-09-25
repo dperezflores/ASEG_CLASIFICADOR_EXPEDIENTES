76adcc7fb8f3fb0045efa970288c42e2c5f60e34
dperezflores <daniel.apf99@gmail.com>
2026-09-24 23:07:08 -0600

    Limpiar resultado EST V2 al cambiar expediente

--- pages/1_Expediente.py
+++ pages/1_Expediente.py
@@ -38,6 +38,7 @@ CLAVES_EXPEDIENTE = (
     "resultado_unidad_completa",
     "ficha_documental_experimental",
     "ficha_v2_jev_experimental",
+    "est_completa_ficha_v2_experimental",
     "relaciones_globales_exactas",
     "relaciones_globales_similitud_pdf",
 )
